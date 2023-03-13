ccd53bf05323ec5f21af5be840441967da4645f5
afandy72 <youssefafandy142@gmail.com>
2023-03-13 12:07:56 +0200

    5-View Body

--- lib/views/notesviews.dart
+++ lib/views/notesviews.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_application_1/views/Widgets/notes_view_body.dart';
 
 class NotesViews extends StatelessWidget {
   const NotesViews({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: NotesViewBody(),
+    );
   }
 }
