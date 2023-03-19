abc4ce7d5ab340876a5c09772cf1f5e8bf037bc3
afandy72 <youssefafandy142@gmail.com>
2023-03-19 22:51:55 +0200

    9- Note Tips

--- lib/views/Widgets/notes_view_body.dart
+++ lib/views/Widgets/notes_view_body.dart
@@ -7,13 +7,45 @@ class NotesViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        SizedBox(
-          height: 50,
-        ),
-        CustomAppBar()
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(),
+          NoteItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 115, 105, 0),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(children: [
+        ListTile(
+          title: Text(
+            'Flutter Tips',
+            style: TextStyle(color: Colors.black),
+          ),
+          subtitle:
+              Text('build your career', style: TextStyle(color: Colors.black)),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.delete, color: Colors.black),
+          ),
+        )
+      ]),
     );
   }
 }
