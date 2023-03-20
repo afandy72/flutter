8f5c13c32da51112f6881400961ffd56528fee44
afandy72 <youssefafandy142@gmail.com>
2023-03-20 14:34:02 +0200

    11- CON Padding

--- lib/views/Widgets/notes_view_body.dart
+++ lib/views/Widgets/notes_view_body.dart
@@ -28,6 +28,7 @@ class NoteItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
       decoration: BoxDecoration(
         color: Color.fromARGB(255, 237, 219, 26),
         borderRadius: BorderRadius.circular(16),
@@ -38,17 +39,23 @@ class NoteItem extends StatelessWidget {
             'Flutter Tips',
             style: TextStyle(color: Colors.black, fontSize: 26),
           ),
-          subtitle: Text('build your career',
-              style: TextStyle(color: Colors.black, fontSize: 20)),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(left: 14, top: 16),
+            child: Text('build your career',
+                style: TextStyle(color: Colors.black, fontSize: 20)),
+          ),
           trailing: IconButton(
             onPressed: () {},
             icon: Icon(Icons.delete, color: Colors.black),
           ),
         ),
-        Text(
-          'May21 ,2023',
-          style: TextStyle(
-            color: Colors.black,
+        Padding(
+          padding: const EdgeInsets.only(right: 16),
+          child: Text(
+            'May21 ,2023',
+            style: TextStyle(
+              color: Colors.black,
+            ),
           ),
         )
       ]),
