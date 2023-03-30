0afc6cc2effa9e40bf17abf3bf02b3bd9483a4b8
afandy72 <youssefafandy142@gmail.com>
2023-03-30 14:12:50 +0200

    18

--- lib/views/Widgets/add_note_bottom_sheet.dart
+++ lib/views/Widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/views/Widgets/custom_text_fieeld.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        SizedBox(
+          height: 32,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CustomTextField(),
+        ),
+      ],
+    );
   }
 }
---
+++ lib/views/Widgets/custom_text_fieeld.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+      ),
+    );
+  }
+}
