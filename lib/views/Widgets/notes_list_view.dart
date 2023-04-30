a07c2787b3ab394261df00e2e47c983a1610f179
afandy72 <youssefafandy142@gmail.com>
2023-04-30 17:58:33 +0300

    37

--- lib/views/Widgets/notes_list_view.dart
+++ lib/views/Widgets/notes_list_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/views/Widgets/EditNoteViewBody.dart';
 import 'custom_note_item.dart';
 
 class NotesListView extends StatelessWidget {
@@ -6,51 +7,62 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        padding: EdgeInsets.zero,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Container(
-              padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 237, 219, 26),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child:
-                  Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-                ListTile(
-                  title: Text(
-                    'Tips',
-                    style: TextStyle(color: Colors.black, fontSize: 26),
-                  ),
-                  subtitle: Padding(
-                    padding:
-                        const EdgeInsets.only(left: 14, top: 16, bottom: 16),
-                    child: Text('build your notes',
-                        style: TextStyle(color: Colors.black, fontSize: 20)),
-                  ),
-                  trailing: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.delete,
-                      color: Colors.black,
-                      size: 35,
-                    ),
-                  ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) {
+            return EditNoteViewBody();
+          },
+        ));
+      },
+      child: ListView.builder(
+          padding: EdgeInsets.zero,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Container(
+                padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 237, 219, 26),
+                  borderRadius: BorderRadius.circular(16),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 16),
-                  child: Text(
-                    'May21 ,2023',
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
-                )
-              ]),
-            ),
-          );
-        });
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      ListTile(
+                        title: Text(
+                          'Tips',
+                          style: TextStyle(color: Colors.black, fontSize: 26),
+                        ),
+                        subtitle: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 14, top: 16, bottom: 16),
+                          child: Text('build your notes',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 20)),
+                        ),
+                        trailing: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.delete,
+                            color: Colors.black,
+                            size: 35,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 16),
+                        child: Text(
+                          'May21 ,2023',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      )
+                    ]),
+              ),
+            );
+          }),
+    );
   }
 }
