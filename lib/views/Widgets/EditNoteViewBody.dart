748eac5d65eb0eb8ba69e2c61c1b9b24711ae78d
afandy72 <youssefafandy142@gmail.com>
2023-04-30 20:54:53 +0300

    47

--- lib/views/Widgets/EditNoteViewBody.dart
+++ lib/views/Widgets/EditNoteViewBody.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_application_1/views/Widgets/custom_app_bar.dart';
+import 'package:flutter_application_1/views/Widgets/custom_text_fieeld.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -18,6 +19,17 @@ class EditNoteViewBody extends StatelessWidget {
               CustomAppBar(
                 eee: "Edit Note",
                 icon: Icons.check,
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              CustomTextField(hinttext: "Title"),
+              SizedBox(
+                height: 32,
+              ),
+              CustomTextField(
+                hinttext: "content",
+                maxlines: 5,
               ),
             ],
           ),
