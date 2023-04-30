a6f31e2943eb4b96c74d29e35175e16d7efb9128
afandy72 <youssefafandy142@gmail.com>
2023-04-30 22:22:52 +0300

    61

--- lib/views/Widgets/custom_text_fieeld.dart
+++ lib/views/Widgets/custom_text_fieeld.dart
@@ -3,12 +3,20 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hinttext, this.maxlines = 1});
+  const CustomTextField(
+      {super.key, required this.hinttext, this.maxlines = 1, this.onSaved});
   final String hinttext;
   final int maxlines;
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return "Field is required";
+        }
+      },
       maxLines: maxlines,
       decoration: InputDecoration(
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
