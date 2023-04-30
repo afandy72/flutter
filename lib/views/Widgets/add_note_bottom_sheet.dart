496cf39edc7837b6273bd0cfbb7b520ba4327c38
afandy72 <youssefafandy142@gmail.com>
2023-04-30 17:42:20 +0300

    34

--- lib/views/Widgets/add_note_bottom_sheet.dart
+++ lib/views/Widgets/add_note_bottom_sheet.dart
@@ -8,42 +8,45 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            hinttext: "Title",
-          ),
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            hinttext: "Note",
-            maxlines: 9,
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          TextButton(
-              onPressed: () {},
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: 50,
-                decoration: BoxDecoration(
-                    color: Colors.blue, borderRadius: BorderRadius.circular(8)),
-                child: Center(
-                  child: Text(
-                    " Add",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hinttext: "Title",
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hinttext: "Note",
+              maxlines: 9,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextButton(
+                onPressed: () {},
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Center(
+                    child: Text(
+                      " Add",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-              ))
-        ],
+                ))
+          ],
+        ),
       ),
     );
   }
