10e0cd1cf6ba8d101b5835ce447539689031170e
afandy72 <youssefafandy142@gmail.com>
2023-05-01 21:51:15 +0300

    70

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,11 +1,17 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter_application_1/models/note_model.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+
+import '../../constant.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
-  addNote(NoteModel note) {}
+  addNote(NoteModel note) async {
+    var notesBox = Hive.box(knotesbox);
+    await notesBox.add(note);
+  }
 }
